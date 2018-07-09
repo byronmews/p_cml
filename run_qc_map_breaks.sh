d4fd8a55266c920ce63a13606683438a7cda3b95
graham <malikian@login-3-internal.cx1.hpc.ic.ac.uk>
2018-07-09 13:43:32 +0100

    Added BreakDancer and QC stats modules

--- run_qc_map_breaks.sh
+++ run_qc_map_breaks.sh
@@ -7,18 +7,20 @@
 # Description: Breakpoint mapping analysis
 #----------------------------------------------------
 
-########## Load modules ##########
+########## Load modules
 module load fastqc # defaults to v.X
 module load trimmomatic/0.36
 module load bwa/0.7.15
-module load crest/1.0 
+module load crest/1.0 # requires Bio:DB:Sam module, and an aligner eg.
 module load blat/35
-module load samtools/1.2
+module load samtools/1.2 # dependencie for CREST? v.0.1.17 maybe needed
 module load bioperl/1.6.0
 module load picard/2.6.0
+module load breakdancer/1.2
 
-########### Static vars ##########
+########## Static vars ##########
 DBS="/home/malikian/bp_mapping/dbs"
+outputDir="results"
 
 
 ######### Functions/Modules ##########
@@ -63,12 +65,48 @@ func_bwa() {
 	samtools index $fastq_base_name".bam"
 }
 
-#
-# CREST
-#func_crest() {
-#	# Crest
-#
-#}
+func_mapping_stats() {
+
+	# Basic mapping QC
+        echo "Mapping QC stats..."
+        samtools flagstat $fastq_base_name".bam" > $fastq_base_name".bam.flagstat.txt"
+        samtools idxstats $fastq_base_name".bam" > $fastq_base_name".bam.idxstats.txt"
+        samtools stats $fastq_base_name".bam" > $fastq_base_name".bam.stats.txt"
+}
+
+
+# CREST. As extractSclip.pl
+func_crest() {
+	#
+	echo "Running CREST..."
+	
+	echo "Can't locate Bio/DB/Sam.pm"	
+
+}
+
+# BREAKDANCER. As bam2cfg.pl>breakdancer-max
+func_breakdancer() {
+	echo "Detecting inter-chromosomal translocations..."
+
+	# Setup results dir, * add ignore is present
+	if [ -d "$outputDir" ]; then
+  		echo "Results folder already exists"
+	else
+		echo "Creating output results folder"
+		mkdir $outputDir
+	fi
+
+	# bam2cfg.pl -g -h 120925_M00368_0055_A000000000-A1EY4_CGATGT_A_S1.bam > *.cfg
+        echo "Building configuration file for BreakDancer..."
+	bam2cfg.pl -g -h $fastq_base_name".bam" > $fastq_base_name".bam.cfg"
+
+	# breakdancer-max
+	echo "Running BreakDancer..."
+	echo "Output in path:" $outputDir"/"$fastq_base_name".breakdancer.ctx"
+	breakdancer_max $fastq_base_name".bam.cfg" > $outputDir"/"$fastq_base_name".breakdancer.ctx"
+
+}
+
  
 
 ########## Main logic ########## 
@@ -77,7 +115,7 @@ func_bwa() {
 r1=120925_M00368_0055_A000000000-A1EY4_CGATGT_A_S1_L001_R1_001.fastq.gz
 r2=120925_M00368_0055_A000000000-A1EY4_CGATGT_A_S1_L001_R2_001.fastq.gz
 
-# Uncomment the below args handling if removing filenames hardset above.
+# Remove commentsif using cmd line args. Remove above hardset filenames
 #Check input for 2 filein
 #if [ $# -ne 2 ]; then 
 #	echo "Enter 2 fastq files"
@@ -92,23 +130,27 @@ r2=120925_M00368_0055_A000000000-A1EY4_CGATGT_A_S1_L001_R2_001.fastq.gz
 	fastq_base_name=`echo $fastq_r1 | sed 's/_L001_R.*//'` # core filename
 
 	# 1.Run fastqQC pre trim
-	func_fastqc $fastq_r1 $fastq_r2
+	#func_fastqc $fastq_r1 $fastq_r2
 	
-	# 2.Run trimming
+	# 2.Run trimming. Creates trimmed vars.
 	func_trimmomatic $fastq_r1 $fastq_r2
 	
 	# 3.Run fastQC post trim. Note variables assigned in trimming function.
-	func_fastqc $fastq_r1_trimmed $fastq_r2_trimmed
+	#func_fastqc $fastq_r1_trimmed $fastq_r2_trimmed
 
-	# 4.Run mapping
-	func_bwa $fastq_r1_trimmed $fastq_r2_trimmed
+	# 4.Run mapping and QC
+	#func_bwa $fastq_r1_trimmed $fastq_r2_trimmed
+	func_mapping_stats
 	
 	# 5.Run CREST
 	#func_crest
 
+	# 6.Run BREAKDANCER
+	func_breakdancer	
+
 	echo "Done"
 
-# Remove comment if using cmd args
+# Remove comment if using cmd args loop str
 #fi
 
 
