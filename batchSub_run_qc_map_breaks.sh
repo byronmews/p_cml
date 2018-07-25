472b3814e8ce4d3c9cbfb0d960e49b63134f47c5
graham <malikian@login-2-internal.cx1.hpc.ic.ac.uk>
2018-07-25 15:56:39 +0100

    batch sub update

--- batchSub_run_qc_map_breaks.sh
+++ batchSub_run_qc_map_breaks.sh
@@ -19,12 +19,13 @@ cat $PBS_NODEFILE
 echo --------------------------------------
 
 ########## Load modules
-module load fastqc # defaults to v.X
+module load fastqc/0.11.5 # defaults to v.X
 module load trimmomatic/0.36
 module load bwa/0.7.15
 module load picard/2.6.0
 module load breakdancer/1.2
 module load anaconda3/personal # for CRESt and dependencies
+module load blat/35
 ########## End of load modules
 
 
